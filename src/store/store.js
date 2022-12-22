a9931c4ca8a60f63cbd79032c311b2cc6f7a80a5
zhahrany <zhahrany@gmail.com>
2022-12-23 03:20:22 +0700

    hal yg kurang penting

--- src/store/store.js
+++ src/store/store.js
@@ -1,46 +1,46 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 export function useStorage(key, initialValue) {
-    let storedValue = JSON.parse(localStorage.getItem(key))
+  let storedValue = JSON.parse(localStorage.getItem(key));
 
-    let store = writable(storedValue ? storedValue : initialValue)
+  let store = writable(storedValue ? storedValue : initialValue);
 
-    store.subscribe(value => localStorage.setItem(key, JSON.stringify(value)))
+  store.subscribe((value) => localStorage.setItem(key, JSON.stringify(value)));
 
-    return store
+  return store;
 }
 
-export const favStatus = writable('all')
+export const favStatus = writable("all");
 
-export const orderByStatus = writable('latest')
+export const orderByStatus = writable("latest");
 
-export const filter = writable('')
+export const filter = writable("");
 
 function createFilteredNotes() {
-  const { subscribe, set, update } = writable([])
+  const { subscribe, set, update } = writable([]);
 
   return {
     subscribe,
-    applyFilter: (filter, notes) => update((filteredNotes) => {
-      switch (filter) {
-        case 'all-latest':
-          filteredNotes = notes
-          break
-        case 'all-oldest':
-          filteredNotes = notes.filter(note => note).reverse()
-          break
-        case 'favorite-latest':
-          filteredNotes = notes.filter(note => note.favorite)
-          break
-        case 'favorite-oldest':
-          filteredNotes = notes.filter(note => note.favorite).reverse()
-          break
-      }
-      return filteredNotes
-    }
-    ),
-    change: (notes) => set(notes)
-  }
+    applyFilter: (filter, notes) =>
+      update((filteredNotes) => {
+        switch (filter) {
+          case "all-latest":
+            filteredNotes = notes;
+            break;
+          case "all-oldest":
+            filteredNotes = notes.filter((note) => note).reverse();
+            break;
+          case "favorite-latest":
+            filteredNotes = notes.filter((note) => note.favorite);
+            break;
+          case "favorite-oldest":
+            filteredNotes = notes.filter((note) => note.favorite).reverse();
+            break;
+        }
+        return filteredNotes;
+      }),
+    change: (notes) => set(notes),
+  };
 }
 
-export const filteredNotes = createFilteredNotes()
+export const filteredNotes = createFilteredNotes();
